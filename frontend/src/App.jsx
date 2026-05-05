59135d54ead296d8193dbfb06e18d0b42c8ba7ed
Brandon-F316 <bfenelon153@gmail.com>
2026-05-05 13:26:08 -0500

    Setup the Navbar for the website +implemented material UI library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,24 @@ import Home from './components/home'
 import Create from './components/Create'
 import Edit from './components/Edit'
 import Delete from './components/Delete'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar
+        content={
       <Routes>
-        <Route path = '' element = {<Home/>}/>
-        <Route path = '/create' element = {<Create/>}/>
-        <Route path = '/edit/:id' element = {<Edit/>}/>
-        <Route path = '/delete/:id' element = {<Delete/>}/>
+          <Route path = '' element = {<Home/>}/>
+          <Route path = '/create' element = {<Create/>}/>
+          <Route path = '/edit/:id' element = {<Edit/>}/>
+          <Route path = '/delete/:id' element = {<Delete/>}/>
       </Routes> 
+      }
+      
+      />
+
     </>
   )
 }
